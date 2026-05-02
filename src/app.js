5fab0648dd183bc12f7e131f0476a8b9b49756a5
ziyamohammad <253849246+NoCapCode-tm@users.noreply.github.com>
2026-05-03 02:55:48 +0530

    models changed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "http://localhost:3001",
+  "http://localhost:5173",
   "https://nocapcode.cloud",
   "https://atlas.nocapcode.cloud",
 ];
