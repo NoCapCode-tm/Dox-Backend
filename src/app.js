32e0f963befd3f0cbeccf310ad08692b31097a6a
Om Vashishtha <143586605+omvashishtha@users.noreply.github.com>
2026-07-29 21:30:35 +0530

    rm--lh 3000-5173

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:5173",
+  // "http://localhost:3000",
+  // "http://localhost:5173",
   "https://dox.nocapcode.cloud",
   "https://atlas.nocapcode.cloud",
 ];
